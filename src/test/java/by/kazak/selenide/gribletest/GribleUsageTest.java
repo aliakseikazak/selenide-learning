7675c87bb9e54eed67f7f185753045403d4f37db
Aliaksei Kazak <aliaksei_kazak@epam.com>
2019-10-01 11:05:04 +0300

    changed mainpage name to products

--- src/test/java/by/kazak/selenide/gribletest/GribleUsageTest.java
+++ src/test/java/by/kazak/selenide/gribletest/GribleUsageTest.java
@@ -1,14 +1,14 @@
 package by.kazak.selenide.gribletest;
 
 import by.kazak.selenide.gribletest.configs.BaseTest;
-import by.kazak.selenide.gribletest.widgets.MainPage;
+import by.kazak.selenide.gribletest.widgets.Products;
 import org.junit.Test;
 
 public class GribleUsageTest extends BaseTest {
 
     @Test
     public void createTestTableBasedOnDataStorageForNewProduct () {
-        MainPage mainPage = new MainPage();
-        mainPage.open();
+        Products products = new Products();
+        products.open();
     }
 }
--- src/test/java/by/kazak/selenide/gribletest/widgets/Products.java
+++ src/test/java/by/kazak/selenide/gribletest/widgets/Products.java
@@ -2,7 +2,7 @@ package by.kazak.selenide.gribletest.widgets;
 
 import com.codeborne.selenide.Selenide;
 
-public class MainPage {
+public class Products {
 
     public void open () {
         Selenide.open("/");
